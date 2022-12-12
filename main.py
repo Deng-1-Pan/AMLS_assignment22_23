82ccfda0c14eb38e76888ad9bd889ddc87f6a9d7
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-12 23:31:32 +0000

    Data Loading Complete

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import pandas as pd
+from importlib.machinery import SourceFileLoader
 
 import src.landmarks as lm
 
 def Load_data(type):
     if type == 'A':
+        print("==============================Loading data for Task A==============================")
+        # training_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23/celeba/labels.csv', sep='\t', index_col=0)
+        # testing_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23_test/celeba_test/labels.csv', sep='\t', index_col=0)
         features, labels = lm.extract_features_labels()
+        print("==============================Data loading complete==============================")
         return features, labels
     else:
         datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23/cartoon_set/labels.csv', sep='\t', index_col=0)
         return datasets
 
-def solve_A1(datasets):
+def solve_A1():
+    print("==============================Task A1 start to solve==============================")
+    features, labels = Load_data('A')
     
-    return
+    # Supervised feature selection method:
+    # Random forests
+    return None
 
-def solve_A2(datasets):
+def solve_A2():
     
-    return
+    return None
 
 def solve_B1(datasets):
     
-    return
-
+    return None
+ 
 def solve_B2(datasets):
     
-    return
+    return None
 
 def main():
     # For part A
-    features, labels = Load_data('A')
-    solve_A1(features, labels)
-    solve_A2(features, labels)
+    solve_A1()
+    solve_A2()
     
     # For part B
     datasets = Load_data('B')
