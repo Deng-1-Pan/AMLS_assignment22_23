2df5e94b3da11c5c1898472bd336ea5c199d2c90
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-14 17:29:27 +0000

    Refinment + A1_SVM first attampt

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import numpy as np
 from importlib.machinery import SourceFileLoader
 
 import src.landmarks as lm
+from A1.SVM import SVM
+from src.loading import *
 
 
 def Load_data(type):
     if type == "A":
-        print(
-            "==============================Loading data for Task A=============================="
-        )
-        features, labels = [], []
+        print("==============================Loading data for Task A==============================")
+        # training_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23/celeba/labels.csv', sep='\t', index_col=0)
+        # testing_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23_test/celeba_test/labels.csv', sep='\t', index_col=0)
+        # with Loader("Loading the dataset..."):
         features_train, labels_train = lm.extract_features_labels("Train")
         features_test, labels_test = lm.extract_features_labels("Test")
-        features.extend([features_train, features_test])
-        labels.extend([labels_train, labels_test])
-        print(
-            "==============================Data loading complete=============================="
-        )
-        return features, labels
+        print("\n")
+        print("==============================Data loading complete==============================")
+        return features_train, labels_train, features_test, labels_test
     else:
         datasets = pd.read_csv(
             "./Datasets/dataset_AMLS_22-23/cartoon_set/labels.csv",
@@ -29,13 +28,17 @@ def Load_data(type):
 
 
 def solve_A1():
-    print(
-        "==============================Task A1 start to solve=============================="
-    )
-    features, labels = Load_data("A")
-    a = 1
+    print("==============================Task A1 start to solve==============================")
+
+    features_train, labels_train, features_test, labels_test = Load_data("A")
+
     # Supervised feature selection method:
-    # Random forests
+    # SVM
+    A1_SVM_pred = SVM(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])),
+                      labels_train,
+                      features_test.reshape(
+                          (features_test.shape[0], features_test.shape[1]*features_test.shape[2])),
+                      labels_test)
     return None
 
 
