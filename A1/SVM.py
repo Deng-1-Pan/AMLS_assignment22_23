248ac5109da3005de578799207845f0135143a31
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-14 23:21:43 +0000

    SVM Initial completion

--- A1/SVM.py
+++ A1/SVM.py
@@ -1,40 +1,57 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 from sklearn import svm, datasets
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
 from alive_progress import alive_bar
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix
+
 
 
 def SVM(train_data, train_labels, test_data, test_labels):
 
-    # classifier = svm.SVC(kernel='linear')
+    # SVM regularization parameter
+    C = list([1e-3, 1e-2, 0.1, 1, 10, 1e2, 1e3])
 
-    # classifier.fit(train_data, train_label)
-
-    # pred = classifier.predict(test_data)
-
-    # print(pred)
-
-    # print("Accuracy:", accuracy_score(test_label, pred))
-
-    C = 1  # SVM regularization parameter
-    models = np.array([svm.SVC(kernel='rbf', C=C),
-                       svm.SVC(kernel='poly', degree=2, C=C),
-                       svm.SVC(kernel='poly', degree=3, C=C),
-                       svm.SVC(kernel='poly', degree=4, C=C),
-                       svm.SVC(kernel='poly', degree=5, C=C),
-                       svm.SVC(kernel='poly', degree=6, C=C)])
-
-    predicts = []
-    with alive_bar(models.shape[0], force_tty=True, title="Testing", bar="classic", theme="classic") as bar:
-        for clf in models:
-            pred = clf.fit(train_data, train_labels).predict(test_data)
-            predicts.append(accuracy_score(test_labels, pred))
-            print(f"Accuracy for: {clf}", accuracy_score(test_labels, pred))
+    # Tunning the hyperparameter
+    cv_scores = []
+    with alive_bar(len(C), force_tty=True, title='Training', bar='classic', theme='classic') as bar:
+        for c in C:
+            model = svm.LinearSVC(C=c, dual=False)
+            scores = cross_val_score(
+                model, train_data, train_labels, cv=10, scoring='accuracy')
+            cv_scores.append(scores.mean())
             bar()
 
-    print(
-        f"The highest accuracy is {np.max(predicts)} which from {models[np.where(predicts==np.max(predicts))[0][0]]}")
-    print("Done")
+    # plt.plot(range(len(C)), cv_scores)
+    # plt.xlabel('C')
+    # plt.ylabel('Accuracy')
+    # plt.show()
+
+    best_C = C[np.where(cv_scores == max(cv_scores))[0][0]]
+    print(f'The best regularization parameter is {best_C}')
+    
+    # Training the best model
+    best_svm = svm.LinearSVC(C=best_C, dual=False)
+    best_svm.fit(train_data, train_labels)
+    svm_pred = best_svm.predict(test_data)
+    
+    # Validation
+    print('SVC precision : {:.2f}'.format(precision_score(svm_pred, test_labels)))
+    print('SVC recall    : {:.2f}'.format(recall_score(svm_pred, test_labels)))
+    print('Accuracy Score: {:.2f}'.format(accuracy_score(test_labels, svm_pred)))
+    print('SVC f1-score  : {:.2f}'.format(f1_score(svm_pred, test_labels)))
+    
+    confusion_mat = confusion_matrix(test_labels, svm_pred, labels=[1, 0])
+    print('This is the confusion matrix:')
+    plt.figure(num='Confusion Matrix A1 SVM')
+    plt.title('Confusion Matrix A1 SVM')
+    ax = sns.heatmap((confusion_mat / np.sum(confusion_mat)*100),
+                annot=True, fmt=".2f", cmap="crest")
+    ax.set_xlabel('Predicted Label')
+    ax.set_ylabel('True Label')
+    plt.show()
+    
