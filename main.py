0fafe37eb052485e1a6b0c38d0e4fae84e494e87
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-17 15:58:47 +0000

    Refinement

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ from A1.SVM import SVM
 
 def Load_data(type):
     if type == "A":
-        print("==============================Loading data for Task A==============================")
+        print(
+            "==============================Loading data for Task A=============================="
+        )
         # training_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23/celeba/labels.csv', sep='\t', index_col=0)
         # testing_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23_test/celeba_test/labels.csv', sep='\t', index_col=0)
         # with Loader("Loading the dataset..."):
-        features_train, labels_train = lm.extract_features_labels("Train")
+        features_train, train_labels = lm.extract_features_labels("Train")
         features_test, labels_test = lm.extract_features_labels("Test")
         print("\n")
-        print("==============================Data loading complete==============================")
-        return features_train, labels_train, features_test, labels_test
+        print(
+            "==============================Data loading complete=============================="
+        )
+        return features_train, train_labels, features_test, labels_test
     else:
         datasets = pd.read_csv(
             "./Datasets/dataset_AMLS_22-23/cartoon_set/labels.csv",
@@ -27,17 +31,19 @@ def Load_data(type):
 
 
 def solve_A1():
-    print("==============================Task A1 start to solve==============================")
+    print(
+        "==============================Task A1 start to solve=============================="
+    )
 
-    features_train, labels_train, features_test, labels_test = Load_data("A")
+    features_train, train_labels, features_test, test_labels = Load_data("A")
 
     # Supervised feature selection method:
     # SVM
     A1_SVM_pred = SVM(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])),
-                      labels_train,
+                      train_labels,
                       features_test.reshape(
                           (features_test.shape[0], features_test.shape[1]*features_test.shape[2])),
-                      labels_test)
+                      test_labels)
     return None
 
 
