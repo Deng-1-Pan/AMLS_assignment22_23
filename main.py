935757032c51b48043dba243930865f252d0b00a
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-19 18:24:57 +0000

    Refinement of the code format

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import numpy as np
-from importlib.machinery import SourceFileLoader
+# import numpy as np
+# from importlib.machinery import SourceFileLoader
 
 import src.landmarks as lm
 from A1.Training_Models import Model_Training_Testing
@@ -9,16 +9,15 @@ from A1.Training_Models import Model_Training_Testing
 def Load_data(type):
     if type == "A":
         print(
-            "==============================Loading data for Task A=============================="
+            "==============================Loading data for Task A=============================\n"
         )
         # training_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23/celeba/labels.csv', sep='\t', index_col=0)
         # testing_datasets = pd.read_csv('./Datasets/dataset_AMLS_22-23_test/celeba_test/labels.csv', sep='\t', index_col=0)
         # with Loader("Loading the dataset..."):
         features_train, train_labels = lm.extract_features_labels("Train")
         features_test, test_labels = lm.extract_features_labels("Test")
-        print("\n")
         print(
-            "==============================Data loading complete=============================="
+            "\n==============================Data loading complete===============================\n"
         )
         return features_train, train_labels, features_test, test_labels
     else:
@@ -36,45 +35,22 @@ def solve_A1():
     )
 
     features_train, train_labels, features_test, test_labels = Load_data("A")
-
+    
+    print(
+        "=========================+====Training the model selected========================="
+    )
     # Supervised feature selection method:
     # Support vector machine (SVM)
-    Model_Training_Testing(
-        features_train.reshape(
-            (features_train.shape[0], features_train.shape[1] * features_train.shape[2])
-        ),
-        train_labels,
-        features_test.reshape(
-            (features_test.shape[0], features_test.shape[1] * features_test.shape[2])
-        ),
-        test_labels,
-        "SVM",
-    )
+    Model_Training_Testing(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])), train_labels,
+                      features_test.reshape((features_test.shape[0], features_test.shape[1]*features_test.shape[2])), test_labels, 'SVM')
     # K-nearest neighbors (KNN)
-    Model_Training_Testing(
-        features_train.reshape(
-            (features_train.shape[0], features_train.shape[1] * features_train.shape[2])
-        ),
-        train_labels,
-        features_test.reshape(
-            (features_test.shape[0], features_test.shape[1] * features_test.shape[2])
-        ),
-        test_labels,
-        "KNN",
-    )
+    Model_Training_Testing(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])), train_labels,
+                      features_test.reshape((features_test.shape[0], features_test.shape[1]*features_test.shape[2])), test_labels, 'KNN')
     # Random forest (RF)
-    Model_Training_Testing(
-        features_train.reshape(
-            (features_train.shape[0], features_train.shape[1] * features_train.shape[2])
-        ),
-        train_labels,
-        features_test.reshape(
-            (features_test.shape[0], features_test.shape[1] * features_test.shape[2])
-        ),
-        test_labels,
-        "RF",
-    )
-
+    Model_Training_Testing(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])), train_labels,
+                      features_test.reshape((features_test.shape[0], features_test.shape[1]*features_test.shape[2])), test_labels, 'RF')
+    
+    
     return None
 
 
