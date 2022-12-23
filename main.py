9b751f765363def1ac6fe7e01acf70c07a76e1d1
Deng-1-Pan <uceedp1@ucl.ac.uk>
2022-12-23 14:05:00 +0000

    Refinement of the code layout

--- main.py
+++ main.py
@@ -29,28 +29,25 @@ def Load_data(type):
         return datasets
 
 
-def solve_A1():
-    print(
-        "==============================Task A1 start to solve=============================="
-    )
-
-    features_train, train_labels, features_test, test_labels = Load_data("A")
-
-    print(
-        "=========================+====Training the model selected========================="
-    )
+def solve_A1(features_train, train_labels, features_test, test_labels):
     # Supervised feature selection method:
     # Support vector machine (SVM), K-nearest neighbors (KNN), Random forest (RF) and Adaboost
     Model_Options = ['SVM', 'KNN', 'RF', 'AdaBoost']
     for model in Model_Options:
         Model_Training_Testing(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])), train_labels[0],
                                features_test.reshape((features_test.shape[0], features_test.shape[1]*features_test.shape[2])), test_labels[0], model)
-
+    print("==============================Task A1 Completed!==============================")
     return None
 
 
-def solve_A2():
-
+def solve_A2(features_train, train_labels, features_test, test_labels):
+    # Supervised feature selection method:
+    # Support vector machine (SVM), K-nearest neighbors (KNN), Random forest (RF) and Adaboost
+    Model_Options = ['SVM', 'KNN', 'RF', 'AdaBoost']
+    for model in Model_Options:
+        Model_Training_Testing(features_train.reshape((features_train.shape[0], features_train.shape[1]*features_train.shape[2])), train_labels[1],
+                               features_test.reshape((features_test.shape[0], features_test.shape[1]*features_test.shape[2])), test_labels[1], model)
+    print("==============================Task A2 Completed!==============================")
     return None
 
 
@@ -66,8 +63,18 @@ def solve_B2(datasets):
 
 def main():
     # For part A
-    solve_A1()
-    solve_A2()
+    # Loading Data
+    print("==============================Task A1 start to solve==============================")
+    features_train, train_labels, features_test, test_labels = Load_data("A")
+
+    # A1
+    print("==============================Training the model selected=========================")
+    solve_A1(features_train, train_labels, features_test, test_labels)
+
+    # A2
+    print("==============================Task A2 start to solve==============================")
+    print("==============================Training the model selected=========================")
+    solve_A2(features_train, train_labels, features_test, test_labels)
 
     # For part B
     datasets = Load_data("B")
