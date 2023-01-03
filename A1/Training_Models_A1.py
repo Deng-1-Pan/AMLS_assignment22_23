f9cfb18195fbc3458211aaeee913c2f2b294a079
Deng-1-Pan <uceedp1@ucl.ac.uk>
2023-01-03 12:50:02 +0000

    Modify the Code to use the best Model

--- A1/Training_Models_A1.py
+++ A1/Training_Models_A1.py
@@ -1,118 +1,74 @@
-import time
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 from tqdm import tqdm
-from sklearn import svm
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix
 
-random_seed = 0
 
+def Model_Training_Testing_A1(train_data, train_labels, test_data, test_labels):
 
-def model_para(model_index, model, i):
-    if model_index == 0:
-        model.C = i
-    elif model_index == 1:
-        model.n_neighbors = i
-    elif model_index == 2:
-        model.n_estimators = i
-    elif model_index == 3:
-        model.learning_rate = i
+    model = MLPClassifier(alpha=0.0001, hidden_layer_sizes=(
+        130,), random_state=3, max_iter=5000)
 
-    return model
+    # Normalize the data
+    scaler = StandardScaler()
+    train_data = scaler.fit_transform(train_data)
+    test_data = scaler.fit_transform(test_data)
 
+    # Create a KFold generator with 5 folds
+    kfold = KFold(n_splits=5, shuffle=True, random_state=5)
 
-def Model_Select(train_data, model_name):
+    precision_score_list, recall_score_list, accuracy_score_list, f1_score_list = [], [], [], []
+    # Using K-FLod to validate training dataset
+    with tqdm(total=kfold.get_n_splits(train_data), desc="Validating") as pbar:
+        for train_index, test_index in kfold.split(train_data):
+            # Get the training and testing data for this fold
+            X_train, X_test = train_data[train_index], train_data[test_index]
+            y_train, y_test = train_labels[train_index], train_labels[test_index]
 
-    if model_name == 'SVM':
-        model = svm.LinearSVC(dual=False, random_state=random_seed)
-        model_index = 0
-    elif model_name == 'KNN':
-        model = KNeighborsClassifier()
-        model_index = 1
-    elif model_name == 'RF':
-        model = RandomForestClassifier(random_state=random_seed)
-        model_index = 2
-    elif model_name == 'AdaBoost':
-        model = AdaBoostClassifier(random_state=random_seed)
-        model_index = 3
+            # Train and evaluate a model on the training and testing data for this fold
+            model.fit(X_train, y_train)
 
-    # List to store the parameter to be tunned
-    list_para = [[] * 1 for _ in range(4)]
-    # SVM regularization parameter C
-    list_para[0] = list(np.linspace(0.01,10,50))
-    # KNN k parameter
-    list_para[1] = list(range(1, int(np.rint(np.sqrt(len(train_data))))))
-    # RF n_estimators parameter
-    list_para[2] = list(range(268, 274))
-    # Adaboost learning_rate parameter
-    list_para[3] = [1e-4, 1e-3, 1e-2, 0.1, 1]
+            pred = model.predict(X_test)
+            precision_score_list.append(precision_score(pred, y_test))
+            recall_score_list.append(recall_score(pred, y_test))
+            accuracy_score_list.append(accuracy_score(y_test, pred))
+            f1_score_list.append(f1_score(pred, y_test))
+            pbar.update(1)
 
-    return model, model_index, list_para
+    avg_precision = np.mean(precision_score_list)
+    avg_recall = np.mean(recall_score_list)
+    avg_accuracy = np.mean(accuracy_score_list)
+    avg_f1 = np.mean(f1_score_list)
 
+    print(f'\n Avg precision for MLP: {avg_precision:.4f}')
+    print(f'Avg recall for MLP: {avg_recall:.4f}')
+    print(f'Avg accuracy for MLP: {avg_accuracy:.4f}')
+    print(f'Avg f1 score for MLP: {avg_f1:.4f}\n')
 
-def Model_Training_Testing_A1(train_data, train_labels, test_data, test_labels, model_name):
+    # Inference stage
+    pred_test = model.predict(test_data)
 
-    model, model_index, list_para = Model_Select(train_data, model_name)
+    precision = precision_score(pred_test, test_labels)
+    recall = recall_score(pred_test, test_labels)
+    accuracy = accuracy_score(test_labels, pred_test)
+    f1 = f1_score(pred_test, test_labels)
 
-    # Tunning the hyperparameter
-    if model_index != 3:
-        parameter_scores = []
-        with tqdm(total=len(list_para[model_index]), desc="Training on " + model_name) as pbar:
-            for i in list_para[model_index]:
-                train_model = model_para(model_index, model, i)
-                scores = cross_val_score(
-                    train_model, train_data, train_labels, cv=10, scoring='accuracy')
-                parameter_scores.append(scores.mean())
-                pbar.update(1)
-
-        best_para_index = np.where(
-            parameter_scores == max(parameter_scores))[0][0]
-        best_para = list_para[model_index][best_para_index]
-        print(
-            f'The best hyperparameter for {model_name} is {best_para} with average accuracy {parameter_scores[list_para[model_index].index(best_para)]}')
-
-        # Training the best model
-        best_model = model_para(model_index, model, best_para)
-        t1 = time.perf_counter()
-        best_model.fit(train_data, train_labels)
-        t2 = time.perf_counter()
-        print(f"Training Completed! Time used: {t2-t1:.2f}s")
-
-    else:
-        best_model = GridSearchCV(
-            model, {'learning_rate': list_para[3]}, cv=10, scoring='accuracy')
-        t1 = time.perf_counter()
-        best_model.fit(test_data, test_labels)
-        t2 = time.perf_counter()
-        para_name = 'learning_rate'
-        print(
-            f'The best hyperparameter for {model_name} is {best_model.best_params_[para_name]} with average accuracy {best_model.best_score_}')
-        print(f"Training Completed! Time used: {t2-t1:.2f}s")
-
-    # Validation
-    best_model_pred = best_model.predict(test_data)
-    print('{} precision : {:.2f}'.format(model_name,
-          precision_score(best_model_pred, test_labels)))
-    print('{} recall    : {:.2f}'.format(model_name,
-          recall_score(best_model_pred, test_labels)))
-    print('{} Accuracy Score: {:.2f}'.format(model_name,
-          accuracy_score(test_labels, best_model_pred)))
-    print('{} f1-score  : {:.2f}'.format(model_name,
-          f1_score(best_model_pred, test_labels)))
+    print(f'Precision on test dataset for MLP: {precision:.4f}')
+    print(f'Recall on test dataset for MLP: {recall:.4f}')
+    print(f'Accuracy on test dataset for MLP: {accuracy:.4f}')
+    print(f'F1 score on test dataset for MLP: {f1:.4f}\n')
 
     confusion_mat = confusion_matrix(
-        test_labels, best_model_pred, labels=best_model.classes_)
+        test_labels, pred_test, labels=model.classes_)
     print('This is the confusion matrix:')
-    plt.figure(num=f'Confusion Matrix A1 {model_name}')
-    plt.title(f'Confusion Matrix A1 {model_name}')
+    plt.figure(num='Confusion Matrix A1 MLP')
+    plt.title('Confusion Matrix A1 MLP')
     ax = sns.heatmap((confusion_mat / np.sum(confusion_mat)*100),
                      annot=True, fmt=".2f", cmap="crest")
     ax.set_xlabel('Predicted Label')
